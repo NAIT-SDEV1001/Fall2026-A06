480478ac5e3138552f04d1e7e099c36d1fa1a178
Dominic Sabatier <dsabatier@gmail.com>
2026-09-16 08:43:39 -0600

    lesson 7 exercise

---
+++ lesson-07/lists.py
@@ -0,0 +1,13 @@
+ages = [13, 14, 14, 15]
+name = ["Tom", "Sue", "B", "C"]
+
+ages[1] = 13
+name[3] = "Dominic"
+
+
+letters = ['a', 'b', 'c', 'd']
+
+print("First three letters:", letters[:3])
+print(len(letters))
+
+print(letters[10]) # Error!
